24c4f8744a36cf2fcb722967769c6ec3073c423c
Gurinder Chauhan <gurinderchauhan@gmail.com>
2018-07-20 14:41:31 -0400

    wip

---
+++ src/components/Progress.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="progress">
+        <span class="progress-bar"><span class="progress-in" :style="style"></span></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vue-progress-bar",
+
+        props: ['progress'],
+
+        computed: {
+            style() {
+                return {
+                    width: this.progress + '%'
+                }
+            }
+        }
+    }
+</script>
